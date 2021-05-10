67aabbd2622a2516cbd5b813ed03e83c211a3610
Saif Butt <saifibutt1998@gamil.com>
2021-05-10 09:51:36 -0700

    Add file
    added file

---
+++ app.js
@@ -0,0 +1,94 @@
+
+// var a = 15
+// var a = 10
+// a = 5
+// var b = 50
+// var c= "saifi"
+// var saif= "this my name "
+// var saifiBut = "this is my nice name"
+
+// document.write(  )
+
+//  let aa = 15
+// //  let aa = 10 /*this is not possible in let variable */ 
+//  aa = 5
+//  let bb = 50
+//  let cc = "saifi"
+//  let  muneeb= "this my name "
+//  let muneebBut = "this is my nice name"
+
+//  document.write( )
+
+//  const  aaa= 10
+// //  const aaa = 3 this is not work on const variable 
+// //  aaa = 2.5    this is also not work in const variable bcz const variable assing onle one vale this vale is constant throug out our project
+//  const bbb = 15
+//  const ccc = " saifi "
+//  const saifs = " my short name"
+//  const haseebButt = " nice name"
+//  document.write( "  <h1> this is heading tage  ")
+//      document.write(  )
+
+var stirg = " hello world"
+var  de = 5
+var  efg = 4
+var lmn = 2
+var xyz = true 
+  document.write( )
+  
+//  document.write( " <h1> ab </h1> " + "<br>" )
+//  document.write( def)
+//  document.write( "<br>")
+// //  document.write( xyz)
+//  document.write(def + efg + xyz + abc )
+
+// var num = 5
+//  var n =" 4"  
+// console.log( num)
+// // console.log( "< br >")
+// num+=5
+// console.log( num)
+
+// var random = 10 + +('10.5')+30
+// console.log( random) var
+
+// var dahi = false ;
+// var tomoto = false ;
+//  if ( dahi ) {
+//   console.log ( "1 pao dahi dado" );
+//  }
+//   else if ( tomoto) {
+//    console.log (" 1 pao tomoto dado" )
+//  }
+//  else  { 
+//    console.log( "1 Pao dod dado")
+//  } 
+
+var dahi= false;
+var tomoto = false;
+var milk = false
+ if ( dahi) {
+   console.log( "1 pao dahi  dado" )
+  }
+else if ( tomoto) {
+console.log( "1 paoo tomoto dado bhai ")
+}
+
+else if ( milk) {
+  console.log( " 1 pao dod dado")
+}
+else {
+  console.log(" ak kam kar to mara putar hun to choti kar ")
+}
+
+ 
+
+ 
+
+
+
+ 
+
+
+  
+  
